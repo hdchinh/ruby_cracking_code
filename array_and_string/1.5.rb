d31937b48f258f86a4432d28a1379130a1886436
Chinh Hoang <hduychinh@gmail.com>
2019-12-05 09:54:14 +0700

    Update 1.5

--- array_and_string/1.5.rb
+++ array_and_string/1.5.rb
@@ -11,6 +11,16 @@ end
 
 def remove_space(str)
   return str if(count_number_of_space(str) == 0)
+  str_arr = str.split(' ')
+  str_result = ''
+
+  str_arr.each_with_index do |sub_str, index|
+    str_result += sub_str
+
+    str_result += '%20' if (index < str_arr.size - 1)
+  end
+
+  str_result
 end
 
-count_number_of_space("h d c")
+puts remove_space("h d c")
